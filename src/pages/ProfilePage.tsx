c7e4f8eac156016c03ff8ca9d21ce5bf1ce4fe6f
VARUN-M-G <114160763+VARUN-M-G@users.noreply.github.com>
2025-04-25 01:50:28 +0530

    impoting date-fns

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,6 +4,7 @@ import { MainLayout } from "@/components/layout/main-layout";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { isValid } from "date-fns";
 import {
   Card,
   CardContent,
