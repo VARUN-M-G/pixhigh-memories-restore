85548ff583d3d05cfb4305b372f95bb805c8a14e
VARUN-M-G <114160763+VARUN-M-G@users.noreply.github.com>
2025-05-01 03:02:19 +0530

    BG REV1

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,24 +1,10 @@
-
-import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { Footer } from "@/components/Footer";
 
 interface MainLayoutProps {
   children: React.ReactNode;
-  withBackground?: boolean;
 }
 
-export function MainLayout({ children, withBackground = true }: MainLayoutProps) {
-  if (withBackground) {
-    return (
-      <BackgroundGradient className="min-h-screen w-full flex flex-col">
-        <div className="flex-grow">
-          {children}
-        </div>
-        <Footer />
-      </BackgroundGradient>
-    );
-  }
-  
+export function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="min-h-screen bg-background flex flex-col w-full">
       <div className="flex-grow">
