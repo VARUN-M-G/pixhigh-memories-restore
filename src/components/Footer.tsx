39a9ef4706859267aec231ed040a9d277259d50d
VARUN-M-G <114160763+VARUN-M-G@users.noreply.github.com>
2025-05-10 08:27:07 +0530

    Update Footer.tsx PC1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -46,7 +46,7 @@ export function Footer() {
               </a>
             </p>
             <p className="text-muted-foreground">
-              © {new Date().getFullYear()} Varun M G. All rights reserved.
+              © {new Date().getFullYear()} Prajwal C | Tech Mahindra. All rights reserved.
             </p>
           </div>
         </div>
